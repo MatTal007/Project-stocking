67358074d387b78cbe818d2b0cb76488b55897bb
Mateusz Talkowski <fly_master@onet.eu>
2021-03-09 21:18:18 +0100

    stock_quote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    if params[:ticker]
+      @api = StockQuote::Stock.new(api_key: "pk_a18c72beb48d4b7a9c51e0961f9eef3c")
+      @stocks = StockQuote::Stock.quote(params[:ticker])
+    end
+    
   end
+
+  def about
+  end
+
 end
