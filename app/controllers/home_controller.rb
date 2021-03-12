3bb35f72da566041102d231ee85563562806aff2
Mateusz Talkowski <fly_master@onet.eu>
2021-03-12 21:56:18 +0100

    scaffold added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     elsif params[:ticker]
         @api = StockQuote::Stock.new(api_key: "pk_a18c72beb48d4b7a9c51e0961f9eef3c"),
         @stocks = StockQuote::Stock.quote(params[:ticker])
-        if !@stocks
+        if  !@stocks
           @error = "Hey! That Stock Symbol does not exist. Please try again!"
         end
     end
